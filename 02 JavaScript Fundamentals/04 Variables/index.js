6e49c83f0c1e9bd00dd03e63ec5aeb9778c4e343
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-08-06 14:57:05 +0800

    Notes in Variable

--- 02 JavaScript Fundamentals/04 Variables/index.js
+++ 02 JavaScript Fundamentals/04 Variables/index.js
@@ -1,5 +1,10 @@
-let message;
-
-message = "sample lang";
+let message = "juan do";
+let ageNgTao = 9; // applying camelCasing
+let nameNaTripLang = message;
+// valid variable names $ and _
+let $ = 3; 
+let _ = 4;
 
 alert(message);
+alert(nameNaTripLang);
+alert($ + _);
