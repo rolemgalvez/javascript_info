778141b44fc9b175cff1accac792af70de5194a1
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-08-09 08:50:30 +0800

    06 Interation Notes

---
+++ 02 JavaScript Fundamentals/06 Interaction/index.js
@@ -0,0 +1,14 @@
+// alert()
+alert("henlo"); // Modal window with 'OK' option
+
+// prompt()
+result = prompt("What's your favorite number?", [0]); // [] makes value optional
+alert(`Your given value is: ${result}`);
+
+// confirm()
+answer = confirm("Is your day okay?");
+alert(`${answer}`);
+
+// TASK
+name = prompt("What's your name buddy?", "");
+alert(`Oh! Hi ${name}, I'm happy to know you.`);
