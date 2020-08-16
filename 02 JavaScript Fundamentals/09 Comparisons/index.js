35fd299ad8011f367fa4a8bfafba5590f79149d8
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-08-17 07:37:45 +0800

    Comparison with different types

--- 02 JavaScript Fundamentals/09 Comparisons/index.js
+++ 02 JavaScript Fundamentals/09 Comparisons/index.js
@@ -5,10 +5,25 @@ alert(6 != 9); // true (correct)
 
 let result = 69 == 96;
 alert(result); // false
-*/
 
 // Letters are compared using JS "dictionary" or"lexicographical" order
 alert("Z" > "A"); // true
 alert("Hey" < "Hi"); // true
 alert("Hello" == "Henlo"); // false
 alert("B" > "b"); // false
+
+// Comparison of different types: values converted to numbers
+alert("5" > 3); // true
+alert("004" == 4); // true
+alert(true < false); // false, 1 < 0
+alert(true == 1); // true
+
+// Funny Consequence
+let a = 0;
+let b = "0";
+
+alert(a == b); // true
+// Boolean expression use different set of rules
+alert(Boolean(a)); // false
+alert(Boolean(b)); // true
+*/
