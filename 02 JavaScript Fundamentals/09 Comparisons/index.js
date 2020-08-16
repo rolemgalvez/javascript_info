6f47d0557d7eac90841e16b826cf2dae2943adb2
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-08-16 10:26:02 +0800

    08 Comparisons

---
+++ 02 JavaScript Fundamentals/09 Comparisons/index.js
@@ -0,0 +1,14 @@
+/*
+alert(10 > 9); // true (correct)
+alert(1 == 10); // false (wrong)
+alert(6 != 9); // true (correct)
+
+let result = 69 == 96;
+alert(result); // false
+*/
+
+// Letters are compared using JS "dictionary" or"lexicographical" order
+alert("Z" > "A"); // true
+alert("Hey" < "Hi"); // true
+alert("Hello" == "Henlo"); // false
+alert("B" > "b"); // false
