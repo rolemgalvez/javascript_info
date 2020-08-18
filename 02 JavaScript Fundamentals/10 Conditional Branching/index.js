9784c6c46a51490fc8c3683d123d0724a141756b
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-08-18 08:38:43 +0800

    Using curly braces in if statement for readability

--- 02 JavaScript Fundamentals/10 Conditional Branching/index.js
+++ 02 JavaScript Fundamentals/10 Conditional Branching/index.js
@@ -1,2 +1,10 @@
+/*
 let answer = prompt("1 + 1 is?", "");
 if (answer == 2) alert("You're right!");
+*/
+
+let answer = prompt("1 + 1 is?", "");
+if (answer == 2) {
+  alert("You're right!");
+  alert("How amazing of you!");
+}
