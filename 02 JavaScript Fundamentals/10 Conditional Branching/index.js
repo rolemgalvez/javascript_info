affd2a82b81368f28f26dd7b8bab399435a3e52a
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-08-18 08:43:44 +0800

    Boolean values in if statement

--- 02 JavaScript Fundamentals/10 Conditional Branching/index.js
+++ 02 JavaScript Fundamentals/10 Conditional Branching/index.js
@@ -1,10 +1,27 @@
 /*
 let answer = prompt("1 + 1 is?", "");
 if (answer == 2) alert("You're right!");
-*/
 
 let answer = prompt("1 + 1 is?", "");
 if (answer == 2) {
   alert("You're right!");
   alert("How amazing of you!");
+}
+*/
+
+// Boolean conversion
+if (0) {
+  // This will not execute because 0, and also "", null, undefined, NaN, and false are all falsy values
+  alert("You can't see me");
+}
+
+if (1) {
+  // This will execute because it's true
+  alert("You can see me");
+}
+
+let answer = prompt("1 + 1 is?", "");
+let correctAnswer = answer == 2;
+if (correctAnswer) {
+  alert("Hooray!");
 }
