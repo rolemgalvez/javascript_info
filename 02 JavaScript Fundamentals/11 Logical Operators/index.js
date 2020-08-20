0bfafd3449c74e11d9011676f715e547dfc4c5e1
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-08-20 10:44:40 +0800

    OR for getting first true value

--- 02 JavaScript Fundamentals/11 Logical Operators/index.js
+++ 02 JavaScript Fundamentals/11 Logical Operators/index.js
@@ -15,10 +15,22 @@ let hour = 9;
 if (hour < 10 || hour > 18) {
   alert("The office is closed.");
 }
-*/
 
 let hour = 13;
 let isWeekend = true;
 if (hour < 10 || hour > 18 || isWeekend) {
   alert("The office is closed.");
 }
+*/
+
+// OR “||” finds the first truthy value. Return last if all are false
+alert(1 || 0); // 1
+alert(null || 1); // 1
+alert(null || 0 || 1); // 1
+alert(undefined || null || 0); // 0, because no true left
+
+// Using OR to display name from given inputs
+let firstName = "";
+let lastName = "";
+let nickName = "SuperCoder";
+alert(firstName || lastName || nickName || "Anonymous"); // "SuperCoder"
