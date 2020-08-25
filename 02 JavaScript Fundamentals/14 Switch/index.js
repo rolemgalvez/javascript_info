ceb335708dc611eb8fd8551ce91df5709998ffb5
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-08-25 09:12:58 +0800

    Switch

---
+++ 02 JavaScript Fundamentals/14 Switch/index.js
@@ -0,0 +1,30 @@
+/*
+let a = 2 + 2;
+switch (a) {
+  case 3:
+    alert("Too Small");
+    break;
+  case 4:
+    alert("Exactly!");
+    break;
+  case 5:
+    alert("Too Large");
+    break;
+  default:
+    alert("I don't kow such values");
+}
+ */
+
+// Without Break
+let a = 2 + 2;
+switch (a) {
+  case 3:
+    alert("Too Small");
+    break;
+  case 4:
+    alert("Exactly!");
+  case 5:
+    alert("Too Large");
+  default:
+    alert("I don't kow such values");
+}
