ef0434f9725adb3cbc8392b2b254da00d8387e09
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-08-26 19:15:17 +0800

    Task 2

--- 02 JavaScript Fundamentals/14 Switch/index.js
+++ 02 JavaScript Fundamentals/14 Switch/index.js
@@ -69,7 +69,6 @@ switch(number) {
   default:
     alert("No match found");
 }
-*/
 
 // TASK 1: Rewrite the "switch" into an "if"
 let browser = prompt("Enter a browser", "");
@@ -84,4 +83,19 @@ if (browser == "Edge") {
   alert("Alright!");
 } else {
   alert("Is it okay?");
+}
+*/
+
+// TASK 2: Rewrite "if" into "switch"
+let a = +prompt("a?", "");
+switch (a) {
+  case 0:
+    alert(0);
+    break;
+  case 1:
+    alert(1);
+    break;
+  case 2:
+  case 3:
+    alert("2, 3");
 }
