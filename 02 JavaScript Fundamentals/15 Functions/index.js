efa645b471c25409d231492b08291620f711ac6d
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-08-27 09:28:28 +0800

    Local Variable

--- 02 JavaScript Fundamentals/15 Functions/index.js
+++ 02 JavaScript Fundamentals/15 Functions/index.js
@@ -1,7 +1,17 @@
-/* */
+/* 
 function showMessage() {
   alert("Hello everyone");
 }
 
 showMessage();
 showMessage();
+*/
+
+// Local Variable
+function showMessage() {
+  let message = "Henlo po";
+  alert(message);
+}
+
+showMessage();
+alert(message); // Error
