d35feab71da437db9b382898ce522a08585319c6
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-08-30 09:29:27 +0800

    Copying Function

--- 02 JavaScript Fundamentals/16 Function Expressions/index.js
+++ 02 JavaScript Fundamentals/16 Function Expressions/index.js
@@ -1,12 +1,23 @@
-/* */
+/*
 // Function Declaration'
 function sayHi() {
   alert("Hello");
 }
 
-// // Function Expression
-// let sayHi = function () {
-//   alert("Hello");
-// };
+// Function Expression
+let sayHi = function () {
+  alert("Hello");
+};
 
 alert(sayHi);
+ */
+
+// Copying Function
+function sayHi() {
+  alert("Hello");
+}
+
+let func = sayHi;
+
+func(); // Hello
+sayHi(); // Hello
