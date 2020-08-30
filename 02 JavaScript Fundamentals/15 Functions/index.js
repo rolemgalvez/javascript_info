c79e9a2220aa36558389d69c363fd5e798cc2c8a
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-08-30 09:19:14 +0800

    Using for loop in Task 4

--- 02 JavaScript Fundamentals/15 Functions/index.js
+++ 02 JavaScript Fundamentals/15 Functions/index.js
@@ -214,11 +214,16 @@ function min(a, b) {
 }
 
 alert(min(11, 2));
+
 */
 
 // TASK 4: Function pow(x,n)
 function pow(x, n) {
-  return x ** n;
+  let result = x;
+  for (let i = 1; i < n; i++) {
+    result *= x;
+  }
+  return result;
 }
 
 let x = prompt("Enter value for x", 0);
