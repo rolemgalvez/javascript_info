6204f922b58d07d1bebddedf091f2b29481f3a8d
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-08-31 12:41:26 +0800

    No argument

--- 02 JavaScript Fundamentals/17 Arrow Functions/index.js
+++ 02 JavaScript Fundamentals/17 Arrow Functions/index.js
@@ -5,10 +5,14 @@ let sum = (a, b) => a + b;
 //   return a + b;
 // };
 alert(sum(1, 2)); // 3
-*/
 
 // Having only one argument
 let double = (n) => n * 2;
 // same as:
 // let double = function(n) { return n * 2; }
 alert(double(4)); // 8
+*/
+
+// No argument
+let sayHi = () => alert("Hello");
+sayHi();
