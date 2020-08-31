fdc41b8a8125ad89d291edda42ac4d2da5a55b71
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-08-31 12:51:21 +0800

    Arrow in Expression

--- 02 JavaScript Fundamentals/17 Arrow Functions/index.js
+++ 02 JavaScript Fundamentals/17 Arrow Functions/index.js
@@ -11,8 +11,15 @@ let double = (n) => n * 2;
 // same as:
 // let double = function(n) { return n * 2; }
 alert(double(4)); // 8
-*/
 
 // No argument
 let sayHi = () => alert("Hello");
 sayHi();
+*/
+
+// Arrow Function same way as Function Expression
+let age = +prompt("How old are you?", 18);
+
+let welcome = age < 18 ? () => alert("Hello!") : () => alert("Greetings!");
+
+welcome();
