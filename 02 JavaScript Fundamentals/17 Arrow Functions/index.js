3309d6499c33d6668a49a93a0fd6d5899044b06d
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-08-31 12:40:17 +0800

    Having one argument

--- 02 JavaScript Fundamentals/17 Arrow Functions/index.js
+++ 02 JavaScript Fundamentals/17 Arrow Functions/index.js
@@ -1,7 +1,14 @@
-/* */
+/* 
 let sum = (a, b) => a + b;
 // short version for this:
 // let sum = function (a, b) {
 //   return a + b;
 // };
 alert(sum(1, 2)); // 3
+*/
+
+// Having only one argument
+let double = (n) => n * 2;
+// same as:
+// let double = function(n) { return n * 2; }
+alert(double(4)); // 8
