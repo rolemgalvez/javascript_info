49a5f5a3f937ff23bd69f4e41326b9623589fdc2
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-09-03 13:03:48 +0800

    Practice for long string

--- 03 Code Quality/02 Coding Style/index.js
+++ 03 Code Quality/02 Coding Style/index.js
@@ -5,3 +5,10 @@ if (n < 0) {
   alert(`Power ${n} is not supported`);
 }
 
+// Line Length
+let str = `
+  This is sample paragraph presenting the distribution of long horizontal
+  value into a separated context through row partitioning based on your
+  preferred division. This is possible with backtick quotes.
+`;
+
