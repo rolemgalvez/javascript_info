05984a1feeb17be22b37a82c2f739e7ae3d6447c
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-10-01 10:01:13 +0800

    before/after & beforeEach/afterEach

--- 03 Code Quality/05 Testing with Mocha/test.js
+++ 03 Code Quality/05 Testing with Mocha/test.js
@@ -3,6 +3,12 @@ let assert = chai.assert;
 
 describe("pow", function () {
   describe("raises x to power of 3", function () {
+    before(() => alert("BEFORE tests start"));
+    after(() => alert("AFTER tests start"));
+
+    beforeEach(() => alert("BEFORE EVERY test starts"));
+    afterEach(() => alert("AFTER EVERY test starts"));
+
     function makeTest(x) {
       let expected = x * x * x;
       it(`${x} in the power of 3 is ${expected}`, function () {
