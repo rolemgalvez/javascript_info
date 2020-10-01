c01bb07617ee13211b417b7a8e4480e3ecc7397a
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-10-01 09:44:04 +0800

    Nested describe

--- 03 Code Quality/05 Testing with Mocha/test.js
+++ 03 Code Quality/05 Testing with Mocha/test.js
@@ -2,16 +2,18 @@ mocha.setup("bdd");
 let assert = chai.assert;
 
 describe("pow", function () {
-  function makeTest(x) {
-    let expected = x * x * x;
-    it(`${x} in the power of 3 is ${expected}`, function () {
-      assert.equal(pow(x, 3), expected);
-    });
-  }
+  describe("raises x to power of 3", function () {
+    function makeTest(x) {
+      let expected = x * x * x;
+      it(`${x} in the power of 3 is ${expected}`, function () {
+        assert.equal(pow(x, 3), expected);
+      });
+    }
 
-  for (let x = 1; x <= 5; x++) {
-    makeTest(x);
-  }
+    for (let x = 1; x <= 5; x++) {
+      makeTest(x);
+    }
+  });
 });
 
 mocha.run();
