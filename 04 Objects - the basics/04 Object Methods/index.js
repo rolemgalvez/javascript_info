6c294320f700d8456897ca59cc1a89d01b934394
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-10-04 13:47:49 +0800

    Last part in Object Method

--- 04 Objects - the basics/04 Object Methods/index.js
+++ 04 Objects - the basics/04 Object Methods/index.js
@@ -41,7 +41,6 @@ let user = {
 
 user.sayHi();
 
-*/
 // using "this" for a function used by different objects
 let user1 = { name: "Pedro" };
 let user2 = { name: "Raquel" };
@@ -56,3 +55,58 @@ user2.func = sayHi;
 user1.func(); // "Pedro"
 user2.func(); // "Raquel"
 user2["func"](); // "Raquel"
+
+// Arrow functions have no "this"
+let user = {
+  firstName: "Pedro",
+  sayHi() {
+    let arrow = () => alert(this.firstName);
+    arrow();
+  },
+};
+
+user.sayHi(); // Pedro
+
+// Task 2: Create a calculator
+let calculator = {
+  read() {
+    this.val1 = +prompt("Enter value 1", 0);
+    this.val2 = +prompt("Enter value 2", 0);
+  },
+  sum() {
+    return this.val1 + this.val2;
+  },
+  mul() {
+    return this.val1 * this.val2;
+  },
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
+*/
+
+// Task 3: Chaining
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep: function () {
+    alert(this.step);
+    return this;
+  },
+};
+
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); // 1
+
+ladder.step = 0;
+ladder.up().up().down().showStep(); // 1
