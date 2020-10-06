511404ab3ddae2aee1246ad7449ae1260c23d657
Mark Rolem Galvez <rolemgalvez@gmail.com>
2020-10-06 13:42:28 +0800

    Constructor & operator new

--- 04 Objects - the basics/05 Constructor & operator new/index.js
+++ 04 Objects - the basics/05 Constructor & operator new/index.js
@@ -58,5 +58,62 @@ function smallUser() {
 
 alert(new smallUser().name); // Ben
 
+// Methods in Constructor
+function User(name) {
+  this.name = name;
+
+  this.sayHi = function () {
+    alert("My name is " + this.name);
+  };
+}
+
+let john = new User("John");
+john.sayHi();
+
+// Two functions 1 object
+let obj = {};
+
+function A() {
+  return obj;
+}
+function B() {
+  return obj;
+}
+
+alert(new A() == new B()); // true
+
+// Create new calculator
+function Calculator() {
+  this.read = function () {
+    this.n1 = +prompt("Enter 1st number", 0);
+    this.n2 = +prompt("Enter 2nd number", 0);
+  };
+  this.sum = function () {
+    return this.n1 + this.n2;
+  };
+  this.mul = function () {
+    return this.n1 * this.n2;
+  };
+}
+
+let calculator = new Calculator();
+calculator.read();
+
+alert("Sum=" + calculator.sum());
+alert("Mul=" + calculator.mul());
+
 */
-git
+// Create new Accumulator
+function Accumulator(startingValue) {
+  this.value = startingValue;
+  this.read = function () {
+    this.value += +prompt("Enter number", 0);
+  };
+}
+
+let accumulator = new Accumulator(1); // initial value 1
+
+accumulator.read(); // adds the user-entered value
+accumulator.read(); // adds the user-entered value
+
+alert(accumulator.value); // shows the sum of these values
